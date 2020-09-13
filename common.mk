583102faafb1f1c5851e74d37c65c86de1d1566c
Jyotiraditya <imjyotiraditya@pm.me>
2020-09-13 06:04:58 +0000

    sdm856-common: build libhwbinder and copy some prebuilts from vndk29
    Test: build, ril
    Signed-off-by: Jyotiraditya <imjyotiraditya@pm.me>

--- common.mk
+++ common.mk
@@ -85,6 +85,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
 
+
 # OnePlus Camera HIDL
 #PRODUCT_PACKAGES += \
 #    vendor.oneplus.camera.CameraHIDL@1.0 \
@@ -104,6 +105,16 @@ PRODUCT_PACKAGES += \
 # PRODUCT_COPY_FILES += \
 #    $(LOCAL_PATH)/prebuilts/etc/permissions/privapp-permissions-oem.xml:system/etc/permissions/privapp-permissions-oem.xml \
 #    $(LOCAL_PATH)/prebuilts/etc/sysconfig/hiddenapi-package-whitelist-oneplus.xml:system/etc/sysconfig/hiddenapi-package-whitelist-oneplus.xml
+
+# Binder
+PRODUCT_PACKAGES += \
+    libhwbinder \
+    libhwbinder.vendor \
+    libhidltransport
+
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite.so
 
 # Common init scripts
 PRODUCT_PACKAGES += \
